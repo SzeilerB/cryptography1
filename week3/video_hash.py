2c9e0e4296b4ba19aae1c705212fb7541a588273
SzeilerB <balasus07@gmail.com>
2016-11-13 17:19:11 +0100

    Comment and refactor.

--- week3/video_hash.py
+++ week3/video_hash.py
@@ -1,13 +1,6 @@
 from Crypto.Hash import SHA256
 
-
-def main():
-    video_chunks = list(bytes_from_file('video.mp4', 1024))
-
-    for i in range(len(video_chunks)-1, 0, -1):
-        video_chunks[i-1] += SHA256.new(video_chunks[i]).digest()
-
-    return SHA256.new(video_chunks[0]).hexdigest()
+# Calculate blocked hash of a video file using SHA256
 
 
 def bytes_from_file(filename, chunksize):
@@ -20,4 +13,13 @@ def bytes_from_file(filename, chunksize):
                 break
 
 
-print(main())
+def calculate_hash(video_chunks):
+    for i in range(len(video_chunks)-1, 0, -1):
+        video_chunks[i-1] += SHA256.new(video_chunks[i]).digest()
+
+    return SHA256.new(video_chunks[0]).hexdigest()
+
+
+if __name__ == "__main__":
+    print(calculate_hash(list(bytes_from_file('video.mp4', 1024))))
+
